89e579c5cef958cd1145214ac4b9afae0acfb9a3
sayjun0505 <129981240+sayjun0505@users.noreply.github.com>
2023-04-23 07:12:11 -0700

    Update demoJSDOM.js

--- demoJSDOM.js
+++ demoJSDOM.js
@@ -27,12 +27,4 @@ JSDOM.fromURL(url_tagrget).then(dom =>{
     }, 49);      // wait for 49 minutes to make sure html has been completed rendered 
 });  
 
-can you pay for me outside?
-I cannot receive your money in this site.
-There is special case.
-As you know, now my account is not ready payment verify.
-So I asked you.
-If it is possible , please chat followings:
-live:.cid.7b71ea7b875eed59
-https://t.me/greengreen12334
-sayjun0505@gmail.com
+
